8608642c04fc330c99859844e5c1fe208d69c03e
jinzhen38 <jinphto@gmail.com>
2025-08-19 07:57:29 +0900

    todolist

---
+++ simple_project/todolist/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import "./Header.css";
+import { memo } from "react";
+
+const Header = () => {
+    return (
+    <div className="Header"> 
+        <h3>오늘은 🗓️ </h3>
+        <h1>{new Date(). toDateString()}</h1>
+        </div>
+        );
+};
+
+
+// const memoizedHeader = memo(Header);
+// export default Header;
+
+export default memo(Header);
